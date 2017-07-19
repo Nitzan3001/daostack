a087659bcf4b5a7c846752807c8c8ed0958e330f
Jonathan Shahak <zuntah@gmail.com>
2017-07-19 20:19:19 +0300

    DAO Stack JavaScrpit Library - First Commit.

---
+++ jslibrary/daostack.js
@@ -0,0 +1,11 @@
+/*
+DAO Stack JavaScript library
+https://github.com/daostack
+
+Copyright DAO Stack Foundation and other contributors
+
+Date: 2017-07-19
+*/
+
+var arr = [];
+var document = window.document;
